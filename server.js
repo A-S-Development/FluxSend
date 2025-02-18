de75e4bf90c858363a3e7cd6da5647a37e2fe99e
arnoldsanders <arnoldsander@gmail.com>
2025-02-18 18:02:19 -0500

    connect to docker

--- server.js
+++ server.js
@@ -6,14 +6,30 @@ const port = parseInt(process.env.PORT || '3001', 10)
 
 //DB: fluxsend_db
 const client = new Client({
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: 5432,
-    database: process.env.CONNECTION_URL,
-})
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    idleTimeoutMillis: 0,
+    connectionTimeoutMillis: 0
+});
 
-client.connect()
+console.log(client)
+const createDB = async () => {
+  try {
+      await client.connect();
+      console.log("created db")
+  }
+  catch (err) {
+      console.log(err)
+      console.log("Already created")
+  }
+  finally {
+      await client.end();
+  }
+}
+
 // const testConnection = async () => {
 //   await client.connect()
 //   const result = await client.query('SELECT NOW()')
@@ -25,8 +41,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, () => {
-
-
+app.listen(port, async () => {
+  await createDB()
   console.log(`Flux listening on port ${port}`)
 })
